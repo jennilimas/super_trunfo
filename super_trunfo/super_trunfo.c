b1b83fc63737b04564c14023927130ced56c0ee4
Jennifer Lima <limajenni7@gmail.com>
2025-02-27 09:44:02 -0300

    Added Densidade Populacional and PIB per Capita

--- super_trunfo/super_trunfo.c
+++ super_trunfo/super_trunfo.c
@@ -4,11 +4,16 @@
 #include <stdio.h>
 #include <string.h>
 
+//Função principal
 int main(){
     //Declarando variáveis
     char estado, codigo[32], cidade[100];
     int populacao, num_poi;
-    float area, pib;
+    float area, pib, dens_pop, pib_capita;
+
+    printf("Bem-vindo ao Super Trunfo! Um jogo de cartas com o objetivo de determinar qual a mais forte entre os jogadores.\n");
+    printf("Neste jogo, o tema é países, onde cada carta representa uma cidade de um estado.\n");
+    printf("Cadastre as cartas e vamos jogar.\n\n");
 
     //Entrada de dados
     printf("Digite a letra representante do estado (entre A a H): ");
@@ -17,7 +22,7 @@ int main(){
     printf("Digite o código da carta (letra do estado mais um número de 01 a 04): ");
     scanf("%s", codigo);
     //Para remover o caractere de quebra de linha antes da chamada da função fgets e
-    //evitar que seja incluído na entrada de dados
+    //evitar que seja incluído na próxima entrada de dados
     getchar();
 
     printf("Digite o nome da cidade: ");
@@ -38,8 +43,13 @@ int main(){
     printf("Digite a quantidade de pontos turísticos: ");
     scanf("%i", &num_poi);
 
+    //Desafio Nível Aventureiro
+    dens_pop = (float) populacao / area;
+    pib_capita = pib / (float) populacao;
+
     //Apresentando os dados inseridos
     printf("\nCarta: \nEstado: %c\nCódigo: %s\n", estado, codigo);
     printf("Nome da cidade: %s\nPopulação: %d\nÁrea: %.2f km²\n", cidade, populacao, area);
-    printf("PIB: R$ %.2f\nNúmero de pontos turísticos: %i", pib, num_poi);
+    printf("PIB: R$ %.2f\nNúmero de pontos turísticos: %i\n", pib, num_poi);
+    printf("Densidade populacional: %.2f hab/km²\nPIB per Capita: R$ %.2f", dens_pop, pib_capita);
 }
